143111e30665e08b275f1d8fac05fc77b1bba995
EAlbee <albee_eric@hotmail.com>
2015-12-07 20:27:49 -0600

    adding new method for regex parsing and added logging

--- src/MadJavaBeta/EmailValidationBeta.java
+++ src/MadJavaBeta/EmailValidationBeta.java
@@ -2,6 +2,7 @@ package MadJavaBeta;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import java.io.IOException;
+import org.apache.log4j.Logger;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
@@ -15,6 +16,7 @@ import javax.ws.rs.Path;
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/EmailValidation")
 public class EmailValidationBeta {
+    private final Logger logger = Logger.getLogger(this.getClass());
 
     // The Java method will process HTTP GET requests
     @GET
@@ -22,22 +24,24 @@ public class EmailValidationBeta {
     @Produces({"application/json", "text/xml"})
     @Path("/json/{emailAddress}")
     public Email isEmailValidJson(@PathParam("emailAddress") String emailAddress) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Matcher m = null;
+        //String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        //java.util.regex.Matcher m = null;
         Email email = new Email();
         try {
 
             email.setEmailAdress(emailAddress);
 
 
-            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-            m = p.matcher(email.getEmailAdress());
+            //java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+            //m = p.matcher(email.getEmailAdress());
+            //m = parseRegex(emailAddress);
 
-            email.setIsValid(m.matches());
+            email.setIsValid(parseRegex(emailAddress));
 
             return email;
 
         } catch (Exception e) {
+            logger.error("isEmailValidJson", e);
             email.setIsValid(false);
             return email;
         }
@@ -49,27 +53,45 @@ public class EmailValidationBeta {
     @Produces("text/xml")
     @Path("/xml/{emailAddress}")
     public Email isEmailValidHtml(@PathParam("emailAddress") String emailAddress) {
-        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
-        java.util.regex.Matcher m = null;
+//        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        //java.util.regex.Matcher m = null;
         Email email = new Email();
         try {
 
             email.setEmailAdress(emailAddress);
 
 
-            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
-            m = p.matcher(email.getEmailAdress());
+            //java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+            //m = p.matcher(email.getEmailAdress());
+            //m = parseRegex(emailAddress);
 
-            email.setIsValid(m.matches());
+            email.setIsValid(parseRegex(emailAddress));
 
             return email;
 
         } catch (Exception e) {
+            logger.error("isEmailValidHtml", e);
             email.setIsValid(false);
             return email;
         }
     }
 
+    public boolean parseRegex(String emailAddress) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Matcher m = null;
+
+        try {
+
+            java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+            m = p.matcher(emailAddress);
+
+        } catch (Exception e) {
+            logger.error("parseRegex exception", e);
+        } finally   {
+            return m.matches();
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServerFactory.create("http://localhost:9012/");
